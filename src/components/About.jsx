162199b86eb7cd09e01d8e042c5637d60e230a3d
Kabir Hossain <84632624+kabirdkng@users.noreply.github.com>
2024-06-26 23:25:07 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
                       <li>Community Building</li>
                     </ul>
                   <p>Join us on <a href='https://discord.gg/U5EMjSvb4s' target='_blank'>Discord</a> where Head Critter and Mama Critter will be hosting $CRITTER giveaways! We value your input, so let's work together to expand both the community and the Kadena ecosystem. Remember, the stronger we become, the more benefits everyone enjoys!</p>
-                  <p>Leading the charge is Papa Critter, who is fully KYC'd and chats directly witha a couple of guys from Kadena and independant developers. He's passionate about keeping things fun, exciting, and constantly growing, and he's always here to listen to your ideas! :)</p>
+                  <p>Leading the charge is Papa Critter, who is fully KYC'd and chats directly with a couple of guys from Kadena and independant developers. He's passionate about keeping things fun, exciting, and constantly growing, and he's always here to listen to your ideas! :)</p>
               </div>              
             </div>
           </div>
