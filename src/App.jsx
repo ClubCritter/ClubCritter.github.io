795eb63f5f5c41777904f722a48b6f1d434c8185
Kabir Hossain <kabirdkng@gmail.com>
2024-06-26 13:34:55 +0530

    Fixed Loading Issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import bgVideo from './assets/video/gfp-astro-timelapse.mp4'
 import Home from './pages/Home'
@@ -6,6 +6,43 @@ import './App.css'
 import { ClientContextProvider } from './wallet/providers/walletConnect/ClientContextProvider';
 
 function App() {
+    const [allAssetsLoaded, setAllAssetsLoaded] = useState(false);
+  
+    useEffect(() => {
+      const images = document.querySelectorAll('img');
+      const videos = document.querySelectorAll('video');
+      const totalAssets = images.length + videos.length;
+  
+      let loadedAssets = 0;
+  
+      images.forEach((image) => {
+        image.addEventListener('load', () => {
+          loadedAssets++;
+          if (loadedAssets === totalAssets) {
+            setAllAssetsLoaded(true);
+          }
+        });
+      });
+  
+      videos.forEach((video) => {
+        video.addEventListener('loadeddata', () => {
+          loadedAssets++;
+          if (loadedAssets === totalAssets) {
+            setAllAssetsLoaded(true);
+          }
+        });
+      });
+    }, []);
+  
+    useEffect(() => {
+      if (allAssetsLoaded) {
+        setTimeout(() => {
+          document.body.classList.add('loaded');
+        }, 700);
+      }
+    }, [allAssetsLoaded]);
+
+
   return(
     <>
       <video autoPlay muted loop id="bg-video"
--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,8 @@ import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-function BodyClassSetter() {
-  useEffect(() => {
-    setTimeout(() => {
-      document.body.classList.add('loaded');
-    }, 700);
-  }, []);
-
-  return null; // Return null, as this component doesn't render anything
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BodyClassSetter />
     <App />
   </React.StrictMode>,
 );
