a93d500f8a6daf52a7b868aba3b97cdbd25b5fc8
Kabir Hossain <kabirdkng@gmail.com>
2024-07-29 02:17:34 +0530

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <div className="container-fluid footer">
       <footer className="row mx-auto tm-footer">
-        <div className="col-md-6 tm-bg-dark">
+        <div className="col-md-6 tm-bg-dark p-3">
            Copyright 2024 Club Critter - <a href="https://x.com/KadenaCritter"> Cosmic Critter </a> Project _ A <a href='https://www.kadenai.com/' target='_blank'>KadenAi</a> & <a href='https://www.heronheroes.com/welcome' target='_blank'>Heron Heroes</a> Initiative - website developed by <a href='https://x.com/kabir_crypto' target='_blank'>ⓀKabir</a>
         </div>
         <div className="col-md-6 px-0 tm-footer-right">
