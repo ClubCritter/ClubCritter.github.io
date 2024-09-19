e7c0b322ac3fd2a45d90802cb4b12ca1af753c7b
AndyOtter-KDA <andyotterinvestments@gmail.com>
2024-09-19 19:35:52 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
     <div className="container-fluid footer">
       <footer className="row mx-auto tm-footer">
         <div className="col-md-6 tm-bg-dark p-3">
-           Copyright 2024 Club Critter - <a href="https://x.com/KadenaCritter"> Cosmic Critter </a> Project _ A <a href='https://www.kadenai.com/' target='_blank'>KadenAi</a> & <a href='https://www.heronheroes.com/welcome' target='_blank'>Heron Heroes</a> Initiative - website developed by <a href='https://x.com/kabir_crypto' target='_blank'>ⓀKabir</a>
+           Copyright 2024 Club Critter - <a href="https://x.com/KadenaCritter"> Cosmic Critter </a> Project - A <a href='https://kadpad.xyz/' target='_blank'>Kadpad</a> & <a href='https://www.tokengo.fun/' target='_blank'>Tokengo</a> Initiative - website developed by <a href='https://x.com/kabir_crypto' target='_blank'>ⓀKabir</a>
         </div>
         <div className="col-md-6 px-0 tm-footer-right">
           <a href='https://discord.gg/U5EMjSvb4s' target='_blank'>
