cb4cae7c76eb10b39543b9ce5cae685f08ba69b5
AndyOtter <andyotterinvestments@gmail.com>
2025-07-19 23:35:27 +0100

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -11,7 +11,7 @@ const Intro = () => {
                    <h2 className="mb-4">Welcome to Club Critter</h2>
                    <p className="mb-4">The first fully KYC + Audited memecoin made on TokenGo with Kadpad Technology. </p>
                    <h2>Get in on the fun! </h2>
-                   <p>We’re not your average memecoin. We’re the first memecoin launched on TokenGo with Kadpad Technology, making us a unique part of the Kadena ecosystem. We’re a friendly club that coexists peacefully with the entire Kadena community. While we might not have a utility to our token, our passion for helping others grow remains strong. In fact, Papa Otter himself has vouched for Club Critter members’ ability in expanding the Kadena ecosystem. Join us and be part of something amazing. Our leader, Andy Otter, will be actively supporting various initiatives and wants YOU... The community to be apat of it.</p>
+                   <p>We’re not your average memecoin. We’re the first memecoin launched on TokenGo with Kadpad Technology, making us a unique part of the Kadena ecosystem. We’re a friendly club that coexists peacefully with the entire Kadena community. While we might not have a utility to our token, our passion for helping others grow remains strong. In fact, Papa Otter himself has vouched for Club Critter members’ ability in expanding the Kadena ecosystem. Join us and be part of something amazing. Our leader, Andy Otter, will be actively supporting various initiatives and wants YOU... The community to be apart of it.</p>
                  </div>
                 <div className="intro-right">
 		            <div style={{textAlign: 'center'}}>
